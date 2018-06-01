e7f74b639c1e4d2deba0ec971819befa72c785e0
Sara JC Gosline <sara.gosline@sagebase.org>
2018-06-01 09:25:39 -0700

    finished updated code

--- analysis/2018-06-01/reformatRnaSeq.R
+++ analysis/2018-06-01/reformatRnaSeq.R
@@ -21,10 +21,19 @@ all.tpm<-do.call('rbind',lapply(all.dat$`RNASeq Data Gencode`,function(x){
   }
   ))
 
+full.tab<-all.dat%>%rename(id=`RNASeq Data Gencode`)%>%inner_join(all.tpm,by="id")
 #now join into two tidied datasets
-gene.ex.file<-''
 
-nf.pheno.file<-''
+this.script='https://raw.githubusercontent.com/sgosline/pnfCellLines/master/analysis/2018-06-01/reformatRnaSeq.R'
+
+gene.ex.tab<-full.tab%>%select(Sample=`Sample Name`,Gene,Value=tpm)%>%unique()
+write.table(gene.ex.tab,file='updatedNTAPRnaSeqTpmTidied.tsv',sep='\t',row.names=F,col.names=T)
+
+nf.pheno.tab<-full.tab%>%select(Sample=`Sample Name`,Value=`Sample Genotype`)%>%unique()
+nf.pheno.tab$Phenotype=rep('NF1 Genotype',nrow(nf.pheno.tab))
+write.table(nf.pheno.tab,file='NF1GenotypeNtapCellLines.tsv',sep='\t',row.names=F,col.names=T)
 
 parentId='syn8282028'
+synStore(File('updatedNTAPRnaSeqTpmTidied.tsv',parentId=parentId),used=all.dat$`RNASeq Data Gencode`,executed=this.script)
 
+synStore(File('NF1GenotypeNtapCellLines.tsv',parentId=parentId),used=list('syn8397154'),executed=this.script)
